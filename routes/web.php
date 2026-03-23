fea953b97945d8a66097d791fec59b4f0d5cc0e6
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2026-03-23 09:17:50 +0430

    the problem of the si fxed for the system

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // This pulls the FRONTEND_URL from your .env file
+    return redirect(env('FRONTEND_URL', 'http://localhost:3000'));
 });
