2b8a653a589c4cee90be64c9e1f99d92f63476af
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2026-03-23 10:13:06 +0430

    the problem of the si fxed for the system

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -72,6 +72,29 @@ class VideoController extends Controller
             'views' => number_format($v->view_count ?? 0),
         ]);
     }
+     public function getVideoInfo(Request $request)
+    {
+        $url = $request->input('url');
+        
+        // Command to get direct media URLs and metadata in JSON format
+        $command = "yt-dlp --dump-json --no-playlist \"{$url}\"";
+        $result = Process::run($command);
+
+        if ($result->failed()) {
+            return response()->json(['error' => 'Unsupported URL or Video Unavailable'], 400);
+        }
+
+        $data = json_decode($result->output());
+
+        return response()->json([
+            'title' => $data->title,
+            'thumbnail' => $data->thumbnail,
+            'duration' => $data->duration_string,
+            'download_url' => $data->url, // Direct stream link
+            'formats' => collect($data->formats)->where('vcodec', '!=', 'none')->values(),
+        ]);
+    }
+
 
     /**
      * Get "Famous" or Trending videos like Vidmate.
